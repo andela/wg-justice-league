41bc0feb83e26f06fb2c3a55666f8809ba6e267e
PatrickCmd <pwalukagga@gmail.com>
2018-01-18 18:46:39 +0300

    [Feature #154240247] Decide how to present deactivated users in gym overview
    Previously, the only way to determine whether a gym member was active or not was to open his detail view.
    
    This commit adds a tab with navbars for both active and inactive users.

--- wger/exercises/admin.py
+++ wger/exercises/admin.py
@@ -19,6 +19,7 @@ from wger.exercises.models import Exercise
 from wger.exercises.models import ExerciseComment
 from wger.exercises.models import ExerciseCategory
 from wger.exercises.models import Muscle
+from wger.gym.models import Gym
 
 
 class ExerciseCommentInline(admin.TabularInline):  # admin.StackedInline
@@ -34,3 +35,4 @@ admin.site.register(Exercise, ExerciseAdmin)
 admin.site.register(ExerciseCategory)
 admin.site.register(Language)
 admin.site.register(Muscle)
+admin.site.register(Gym)

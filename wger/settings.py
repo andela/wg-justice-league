bfa9f94b00458b69bbfbe2d5c01156bb7d5230c7
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-01-18 22:08:57 +0300

    [Chore #154353945] Update static root.

--- wger/settings.py
+++ wger/settings.py
@@ -64,7 +64,7 @@ WGER_SETTINGS['EMAIL_FROM'] = 'wger Workout Manager <wger@example.com>'
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
   
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static-collected')
 
 STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.

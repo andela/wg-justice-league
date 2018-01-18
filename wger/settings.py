33467b82cb3d3f1e5f4183481a9160d14beda771
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-01-18 21:51:07 +0300

    [Chore #154353945] Add static files base directory.

--- wger/settings.py
+++ wger/settings.py
@@ -68,7 +68,6 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-   os.path.join(PROJECT_ROOT, '/wger/core/static/'),
-)
-  
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+

7940c4711934b872871259a7f6f80514edf3ff64
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-01-18 22:23:14 +0300

    [Chore #154353945] Remove staticfilesdir.

--- wger/settings.py
+++ wger/settings.py
@@ -67,7 +67,3 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(BASE_DIR, 'static-collected')
 
 STATIC_URL = '/static/'
-# Extra places for collectstatic to find static files.
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-

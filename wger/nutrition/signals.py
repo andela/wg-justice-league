1c110136d84bed6a57324b936f53b42a483871e7
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-01-18 09:09:15 +0300

    [Chore #* #154240248] Add comments.

--- wger/nutrition/signals.py
+++ wger/nutrition/signals.py
@@ -17,12 +17,24 @@ from wger.utils.cache import cache_mapper
 
 @receiver([post_save, post_delete], sender=NutritionPlan)
 def np_triggered_cache_reset(sender, instance, **kwargs):
+    '''
+    Delete the nutrition information from the cache when a user
+    adds, edits or deletes a plan.
+    '''
     cache.delete(cache_mapper.get_nutritional_info(instance.pk))
 
 @receiver([pre_save, post_save, post_delete], sender=Meal)
 def meal_triggered_cache_reset(sender, instance, **kwargs):
+    '''
+    Delete the nutrition information from the cache when a user
+    adds, edits or deletes a meal.
+    '''
     cache.delete(cache_mapper.get_nutritional_info(instance.pk))
 
 @receiver([post_save, post_delete], sender=MealItem)
 def meal_item_triggered_cache_reset(sender, instance, **kwargs):
+    '''
+    Delete the nutrition information from the cache when a user
+    adds, edits or deletes a mealitem.
+    '''
     cache.delete(cache_mapper.get_nutritional_info(instance.pk))

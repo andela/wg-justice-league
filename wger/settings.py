d682801ed1d281f8762c27fd91f3cba0466eb356
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-01-19 15:41:56 +0300

    [Chore #154353945] Fix syntax error.

--- wger/settings.py
+++ wger/settings.py
@@ -71,4 +71,4 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '/wger/core/static/'),
+    os.path.join(BASE_DIR, '/wger/core/static/'),)

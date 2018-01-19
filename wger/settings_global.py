069926c4e69996cf198da3e6f3f87bae90e070dd
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-01-19 12:49:04 +0300

    [Chore #154353945] Move static configs from settings.py to settings_global.py

--- wger/settings_global.py
+++ wger/settings_global.py
@@ -312,8 +312,18 @@ THUMBNAIL_ALIASES = {
 #
 # Django compressor
 #
-STATIC_ROOT = ''
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # The default is not DEBUG, override if needed
 # COMPRESS_ENABLED = True

96890460d01e1b01898f1653b359fb6f8666f97f
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-01-19 13:15:35 +0300

    [Chore #154353945] Remove extra places to collect the static file.

--- wger/settings_global.py
+++ wger/settings_global.py
@@ -320,10 +320,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'wger/core/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 # The default is not DEBUG, override if needed
 # COMPRESS_ENABLED = True

69d214c190184dadee866bad0fa80881b9ea7534
Salym Senyonga <salymsash@gmail.com>
2018-01-19 19:17:21 +0300

    [Chore #154240248] Refactor code
    - Commit to using single quotes in `wger/nutrition/apps.py` for
      consistency.
    - Merge the similar methods in `wger/nutrition/signals.py` into one,
      because the *DRY* principle is cool.

--- wger/nutrition/signals.py
+++ wger/nutrition/signals.py
@@ -15,14 +15,9 @@ from wger.nutrition.models import (
 
 from wger.utils.cache import cache_mapper
 
+
 @receiver([post_save, post_delete], sender=NutritionPlan)
-def np_triggered_cache_reset(sender, instance, **kwargs):
-    cache.delete(cache_mapper.get_nutritional_info(instance.pk))
-
 @receiver([pre_save, post_save, post_delete], sender=Meal)
-def meal_triggered_cache_reset(sender, instance, **kwargs):
-    cache.delete(cache_mapper.get_nutritional_info(instance.pk))
-
 @receiver([post_save, post_delete], sender=MealItem)
-def meal_item_triggered_cache_reset(sender, instance, **kwargs):
+def reset_nutrition_info_cache(sender, instance, **kwargs):
     cache.delete(cache_mapper.get_nutritional_info(instance.pk))

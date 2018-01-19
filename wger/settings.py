0256701a5933ccbcc57b5fb8829b52e2827e3227
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-01-19 11:25:28 +0300

    [Chore #154353945] Fix static files.

--- wger/settings.py
+++ wger/settings.py
@@ -62,10 +62,14 @@ WGER_SETTINGS['EMAIL_FROM'] = 'wger Workout Manager <wger@example.com>'
 # Your twitter handle, if you have one for this instance.
 #WGER_SETTINGS['TWITTER'] = ''
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-  
-STATIC_ROOT = os.path.join(BASE_DIR, 'static-collected')
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, '/wger/core/static/'))
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)

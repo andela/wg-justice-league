c9d444ff2ca4784f28aa3052881330931ffcc035
PatrickCmd <pwalukagga@gmail.com>
2018-01-24 18:44:41 +0300

    [Feature #154240250] implement creation of users via rest api

--- wger/core/api/serializers.py
+++ wger/core/api/serializers.py
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Workout Manager.  If not, see <http://www.gnu.org/licenses/>.
 
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 from wger.core.models import (
@@ -24,6 +25,25 @@ from wger.core.models import (
     License,
     RepetitionUnit,
     WeightUnit)
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    '''
+    User creation serializer
+    '''
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
 
 
 class UserprofileSerializer(serializers.ModelSerializer):
